b34ca3ffc6a7422171c9b63433c80c8f78316f08
João Felaço <108115149+Joaofelaco@users.noreply.github.com>
2025-06-04 00:47:00 -0300

    Update users.dart

--- provider/users.dart
+++ provider/users.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:appmae/models/user.dart';
-import '../data/usuarios.dart';
+import '../data/usuarios.dart'; 
 
 class Users with ChangeNotifier {
-  final Map<String, User> _items = dumy();
+  final Map<String, User> _items = dumy(); 
+
   List<User> get all {
     return [..._items.values];
   }
@@ -16,20 +17,18 @@ class Users with ChangeNotifier {
     return _items.values.elementAt(i);
   }
 
+  
   Future<void> put(User user) async {
-    if (user == null) {
-      return;
-    }
+    // if (user == null) { 
+    //   return;
+    // }
 
-    if (user.id != null && //diferente de nulo
-        user.id!.trim().isNotEmpty && //nao estiver vazio
-        _items.containsKey(
-          //estiver contido dentro do map
-          user.id,
-        )) {
+    
+    if (user.id != null &&
+        user.id!.trim().isNotEmpty &&
+        _items.containsKey(user.id)) {
       try {
-        //atualizar o usuario
-        update(user);
+        await update(user); 
         _items.update(
           user.id!,
           (_) => User(
@@ -42,48 +41,47 @@ class Users with ChangeNotifier {
             cpf: user.cpf,
           ),
         );
-      } catch (e) {
+      } catch (e) {        
+        print('Erro ao atualizar usuário: $e');
         return;
       }
     } else {
-      //se nao tiver usuario criar um
+      
       try {
-        int i = 0;
-        insert(user, i);
-        final id = i.toString();
-        _items.putIfAbsent(
-          id,
-          () => User(
-            id: id,
+        
+
+        User userWithPotentiallyNoId = User( 
+            id: null, 
             nome: user.nome,
             email: user.email,
             avatarUrl: user.avatarUrl,
             endereco: user.endereco,
             telefone: user.telefone,
             cpf: user.cpf,
-          ),
         );
+
+        
+        int generatedIdPlaceholder = 0; 
+        await insert(userWithPotentiallyNoId, generatedIdPlaceholder);                                                                
+
       } catch (e) {
+        print('Erro ao inserir usuário: $e');
         return;
       }
     }
-
     notifyListeners();
   }
 
-  void remove(User user) {
-    //deletar o usuario
-    if (user != null && user.id != null) {
+  Future<void> remove(User user) async { 
+    if (user.id != null) {
       try {
-        delete(user.id!);
+        await delete(user.id!); 
         _items.remove(user.id);
         notifyListeners();
       } catch (e) {
+        print('Erro ao excluir usuário: $e');
         return;
       }
     }
   }
 }
-
-//put =>adicionar ou alterar elemento ja existente
-//metodo trim tirar os espaços em branco
